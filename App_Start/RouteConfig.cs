3775491642eb3b7af67e21cfb37208086faf963c
TzelLevanah <lnr.kurai@gmail.com>
2026-02-23 19:02:30 +0700

    First Commit

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AbandonLogger
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // วาง route แบบเฉพาะก่อน
+            routes.MapRoute(
+                name: "AbandonLog",
+                url: "abandon-log",
+                defaults: new { controller = "AbandonLog", action = "Receive" }
+            );
+
+            // Default route ต้องวางไว้ท้ายสุด
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+
+}
---
+++ Controllers/AbandonLogController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace AbandonLogger.Controllers
+{
+    public class AbandonLogModel
+    {
+        public string UserId { get; set; }
+        public string Reason { get; set; }
+        public string Timestamp { get; set; }
+    }
+
+    public class AbandonLogController : Controller
+    {
+        [HttpPost]
+        public ActionResult Receive()
+        {
+            try
+            {
+                Request.InputStream.Position = 0;
+                var json = new StreamReader(Request.InputStream).ReadToEnd();
+
+                var serializer = new JavaScriptSerializer();
+                var data = serializer.Deserialize<AbandonLogModel>(json);
+
+                var logText = $"{data.Timestamp} | {data.UserId} | {data.Reason}{Environment.NewLine}";
+                var path = Server.MapPath("~/App_Data/abandon.log");
+                System.IO.File.AppendAllText(path, logText);
+
+                return new HttpStatusCodeResult(200);
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(500, ex.Message);
+            }
+        }
+    }
+}
